f30e39962f1173d6a54034334d708f7b591fb36e
KainGross <kaingross@gmail.com>
2020-06-24 16:08:43 +0300

    upd1

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="RU">
+<head>
+	<title>Gavno</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<div class="head1">
+			<img src="img/logo.png" alt="logo">
+			<div class="menu"></div>
+		</div>
+		<div class="title1">
+			РЕАЛИЗУЕМ КРУПНЕЙШИЕ ПРОЕКТЫ В РОССИИ
+		</div>
+	</header>
+</body>
+</html>
