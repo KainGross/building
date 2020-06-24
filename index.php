7bff17c81517a6628fe77b99b343503d34722aa2
KainGross <kaingross@gmail.com>
2020-06-25 02:18:31 +0300

    Finish

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <head>
 	<title>Gavno</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="fonts/opensans.css">
+	<link rel="stylesheet" type="text/css" href="style.css?v=3">
 </head>
 <body>
 	<header>
@@ -12,8 +13,228 @@
 			<div class="menu"></div>
 		</div>
 		<div class="title1">
-			РЕАЛИЗУЕМ КРУПНЕЙШИЕ ПРОЕКТЫ В РОССИИ
-		</div>
+			<span>РЕАЛИЗУЕМ КРУПНЕЙШИЕ ПРОЕКТЫ В РОССИИ</span>
+			<p class="desc1">стадионы, газопроводы, мосты, дамбы</p>
+		</div>		
 	</header>
+	<main>
+		<div class="oldfag">
+			<div class="ofitem">
+				<span class="oftitle">26</span>
+				<span class="oft2">лет</span>
+				<span class="ofd">на рынке</span>
+			</div>
+			<div class="ofitem">
+				<span class="oftitle">26</span>
+				<span class="oft2">лет</span>
+				<span class="ofd">на рынке</span>
+			</div>
+			<div class="ofitem">
+				<span class="oftitle">26</span>
+				<span class="oft2">лет</span>
+				<span class="ofd">на рынке</span>
+			</div>
+			<div class="ofitem">
+				<span class="oftitle">26</span>
+				<span class="oft2">лет</span>
+				<span class="ofd">на рынке</span>
+			</div>
+		</div>
+		<div class="biggest">
+			<div class="bcontainer">
+				 <span class="h2title btitle">Наши самые большие проекты</span>
+				 <div class="bitems">
+				 	<div class="bitem">
+				 		<div class="biimg">
+				 			<img src="img/arena.png" alt="arena">
+				 		</div>
+				 		<span class="bititle">Газпром Арена</span>
+				 		<span class="bidesc">Мы сделали самую красивую арену в Европе. Это открытие стало для нас прорывной точкой для разивтия на следующие десятилетия. Мы очень рады данному еву.</span>
+				 	</div>
+				 	<div class="bitem">
+				 		<div class="biimg">
+				 			<img src="img/arena.png" alt="arena">
+				 		</div>
+				 		<span class="bititle">Газпром Арена</span>
+				 		<span class="bidesc">Мы сделали самую красивую арену в Европе. Это открытие стало для нас прорывной точкой для разивтия на следующие десятилетия. Мы очень рады данному еву.</span>
+				 	</div>
+				 	<div class="bitem">
+				 		<div class="biimg">
+				 			<img src="img/arena.png" alt="arena">
+				 		</div>
+				 		<span class="bititle">Газпром Арена</span>
+				 		<span class="bidesc">Мы сделали самую красивую арену в Европе. Это открытие стало для нас прорывной точкой для разивтия на следующие десятилетия. Мы очень рады данному еву.</span>
+				 	</div>
+				 </div>
+			</div>
+		</div>
+		<div class="smart">
+			<div class="stext">
+				<span class="stitle">Самые умные проекты</span>
+				<span class="sdesc">Реализуем самые смелые решения в России!</span>
+			</div>
+			<div class="srequest">
+				<img src="img/letter.png" alt="letter" class="srimg">
+				<span>Ваш запрос</span>
+			</div>
+		</div>
+		<div class="shodelaem">
+			<span class="h2title">Чем мы занимаемся?</span>
+			<div class="quests">
+				<div class="questcont">
+					<div class="quest">
+						<div class="qimg">
+							<img src="img/dom.png" alt="dom">
+						</div>
+						<span class="qtitle">Строительство офисных зданий</span>
+					</div>
+				</div>
+				<div class="questcont">
+					<div class="quest">
+						<div class="qimg">
+							<img src="img/dom.png" alt="dom">
+						</div>
+						<span class="qtitle">Строительство офисных зданий</span>
+					</div>
+				</div>
+				<div class="questcont">
+					<div class="quest">
+						<div class="qimg">
+							<img src="img/dom.png" alt="dom">
+						</div>
+						<span class="qtitle">Строительство офисных зданий</span>
+					</div>
+				</div>
+				<div class="questcont">
+					<div class="quest">
+						<div class="qimg">
+							<img src="img/dom.png" alt="dom">
+						</div>
+						<span class="qtitle">Строительство офисных зданий</span>
+					</div>
+				</div>
+				<div class="questcont">
+					<div class="quest">
+						<div class="qimg">
+							<img src="img/dom.png" alt="dom">
+						</div>
+						<span class="qtitle">Строительство офисных зданий</span>
+					</div>
+				</div>
+				<div class="questcont">
+					<div class="quest">
+						<div class="qimg">
+							<img src="img/dom.png" alt="dom">
+						</div>
+						<span class="qtitle">Строительство офисных зданий</span>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="portfolio">
+		</div>
+		<div class="faq">
+			<p class="h2title">Ответы на вопросы</p>
+			<div class="questions">
+				<div class="question">
+					<p class="qntitle qnclosed">Работаете ли вы по 249 ФЗ?</p>
+					<p class="qnanswer hidden">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, excepturi expedita adipisci cupiditate veniam nemo maxime facilis quos possimus vero eos libero sequi dignissimos nisi assumenda fugit nam accusamus? Labore.</p>
+				</div>
+				<div class="question">
+					<p class="qntitle qnopened">Работаете ли вы по 249 ФЗ?</p>
+					<p class="qnanswer ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, excepturi expedita adipisci cupiditate veniam nemo maxime facilis quos possimus vero eos libero sequi dignissimos nisi assumenda fugit nam accusamus? Labore.</p>
+				</div>
+				<div class="question">
+					<p class="qntitle qnclosed">Работаете ли вы по 249 ФЗ?</p>
+					<p class="qnanswer hidden">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, excepturi expedita adipisci cupiditate veniam nemo maxime facilis quos possimus vero eos libero sequi dignissimos nisi assumenda fugit nam accusamus? Labore.</p>
+				</div>
+				<div class="question">
+					<p class="qntitle qnclosed">Работаете ли вы по 249 ФЗ?</p>
+					<p class="qnanswer hidden">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, excepturi expedita adipisci cupiditate veniam nemo maxime facilis quos possimus vero eos libero sequi dignissimos nisi assumenda fugit nam accusamus? Labore.</p>
+				</div>
+			</div>
+		</div>
+		<div class="partners">
+			<p class="ptitle">Наши партнеры</p>
+			<div class="plist">
+				<img src="img/partner.png" alt="partner">
+				<img src="img/partner.png" alt="partner">
+				<img src="img/partner.png" alt="partner">
+				<img src="img/partner.png" alt="partner">
+				<img src="img/partner.png" alt="partner">
+			</div>
+		</div>
+		<div class="reviews">
+			<p class="h2title">Отзывы о работе с нами</p>
+			<div class="revlist">
+				<div class="review">
+					<div class="revtext">
+						<span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum delectus labore similique ducimus tempora error praesentium sunt, nulla, architecto deserunt tenetur hic, suscipit laborum facere esse illo enim perspiciatis assumenda?</span>
+					</div>
+					<p class="revauthor">
+						<span class="ran">Иванов Иван, </span>
+						<span class="rao">ООО “Газпром-Арена”</span>
+					</p>
+				</div>
+				<div class="review">
+					<div class="revtext">
+						<span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum delectus labore similique ducimus tempora error praesentium sunt, nulla, architecto deserunt tenetur hic, suscipit laborum facere esse illo enim perspiciatis assumenda?</span>
+					</div>
+					<p class="revauthor">
+						<span class="ran">Иванов Иван, </span>
+						<span class="rao">ООО “Газпром-Арена”</span>
+					</p>
+				</div>
+				<div class="review">
+					<div class="revtext">
+						<span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum delectus labore similique ducimus tempora error praesentium sunt, nulla, architecto deserunt tenetur hic, suscipit laborum facere esse illo enim perspiciatis assumenda?</span>
+					</div>
+					<p class="revauthor">
+						<span class="ran">Иванов Иван, </span>
+						<span class="rao">ООО “Газпром-Арена”</span>
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="smart">
+			<div class="stext">
+				<span class="stitle">Хотите с нами работать?</span>
+				<span class="sdesc">Оставьте заявку и мы свяжемся с вами!</span>
+			</div>
+			<div class="srequest">
+				<img src="img/letter.png" alt="letter" class="srimg">
+				<span>Ваш запрос</span>
+			</div>
+		</div>
+		<div class="contacts">
+			<p class="h2title">Контакты</p>
+			<div class="clist">
+				<div class="contact">
+					<div class="cimg"><img src="img/address.png" alt="address"></div>
+					<p class="ctitle">Адрес</p>
+					<p class="ctext">Невский проспект 140,<br>офис 140-142</p>
+				</div>
+				<div class="contact">
+					<div class="cimg"><img src="img/phone.png" alt="phone"></div>
+					<p class="ctitle">Телефон</p>
+					<p class="ctext">8 (812) 111-22-33<br>8 (812) 111-22-33</p>
+				</div>
+				<div class="contact">
+					<div class="cimg"><img src="img/time.png" alt="time"></div>
+					<p class="ctitle">Время работы</p>
+					<p class="ctext">ПН-СБ<br>10:00 - 20:00</p>
+				</div>
+				<div class="contact">
+					<div class="cimg"><img src="img/email.png" alt="email"></div>
+					<p class="ctitle">E-Mail</p>
+					<p class="ctext cmail">INFO@VIP.RU</p>
+				</div>
+			</div>
+		</div>
+	</main>
+	<footer>
+		<span>© 2019 Строительная компания</span>
+		<span>Разработано лучшей студей</span>
+	</footer>
 </body>
 </html>
